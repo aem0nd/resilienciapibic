b80383c63add8505baabec6aecb3e7a7eee65231
aem0nd <123036376+aem0nd@users.noreply.github.com>
2023-01-24 15:27:57 -0300

    Update pr.r

--- pr.r
+++ pr.r
@@ -6,9 +6,50 @@ install.package("readxl")
 
 require(readxl)
 require(tidyverse)
+require(openxlsx)
 
 dadospmu <- read_excel("DOMICILIAR_Consolidados.xlsx")
 matrizod <- read_excel("matrizdistanciapelogmaps.xlsx")
 
 dadospmu <- dadospmu %>%
   mutate(tempoviagem = substr(tempoviagem, 12, 16))
+
+viagensentreras <- dadospmu %>%
+  group_by(RA_origem, RA_destino) %>%
+  summarise(
+    totaldeviagens = n()
+  )
+
+ped <- dadospmu %>% 
+  group_by(modoagregado2, RA_origem, RA_destino) %>%
+  summarise(
+    totaldeviagens = n()
+  ) %>% 
+  filter(modoagregado2 %in% c("TA_aPe"))
+
+cic <- dadospmu %>% 
+  group_by(modoagregado2, RA_origem, RA_destino) %>%
+  summarise(
+    totaldeviagens = n()
+  ) %>% 
+  filter(modoagregado2 %in% c("TA_Bicicleta"))
+
+tc <- dadospmu %>% 
+  group_by(modoagregado1, RA_origem, RA_destino) %>%
+  summarise(
+    totaldeviagens = n()
+  ) %>% 
+  filter(modoagregado1 %in% c("TC"))
+
+ti <- dadospmu %>% 
+  group_by(modoagregado1, RA_origem, RA_destino) %>%
+  summarise(
+    totaldeviagens = n()
+  ) %>% 
+  filter(modoagregado1 %in% c("TI"))
+
+write.xlsx(ped,"dadospmuTape.xlsx",colNames = TRUE)
+write.xlsx(tc,"dadospmuTC.xlsx",colNames = TRUE)
+write.xlsx(ti,"dadospmuTI.xlsx",colNames = TRUE)
+write.xlsx(cic,"dadospmuTbic.xlsx",colNames = TRUE)
+write.xlsx(viagensentreras,"viagnesentreras.xlsx",colNames = TRUE)
